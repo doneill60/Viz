125cf236e5fe1e001bcb984be86757cfa5d7cb91
Zachary Mell <zacharyamell@gmail.com>
2018-02-26 17:09:00 -0500

    added html functionality

--- src/main/java/cs1980viz/CalculateCosine.java
+++ src/main/java/cs1980viz/CalculateCosine.java
@@ -17,62 +17,117 @@ public class CalculateCosine {
 	static final String location = "csv_data";
 	static PriorityQueue<FileData> queue;
 	
-	public static void main (String f1) {
+	public static void main (String matchType, int data) {
+		String fileLocation = "html";
+		//Removing previously generated html data
+		File rm = new File(fileLocation);
+		for(File file:rm.listFiles()){
+			file.delete();
+		}
 		
-		
-		Comparator<FileData> comparator = new FileDataComparator();
-		
-		try{
-			BufferedReader reader = new BufferedReader(new FileReader(f1));
-			String lineToParse;
+		File txt = new File("csv_data");
+		for(File fileName:txt.listFiles()){
 			
-			while((lineToParse = reader.readLine()) != null){
-				String[] parsed = lineToParse.split(",");
-				lv.put(parsed[0], Integer.parseInt(parsed[1]));
-			}
-			reader.close();
+			String f1 = location+"/"+fileName.getName();
+			Comparator<FileData> comparator = new FileDataComparator();
 			
-			File folder = new File(location);
-			File[] list = folder.listFiles();
-			
-			queue = new PriorityQueue<FileData>((list.length-1), comparator);
-			
-			//Comparing file with all other files
-			for (int i = 0; i < list.length; i++){
+			try{
 				
-				//Clearing hashmap
-				rv.clear();
+				BufferedReader reader = new BufferedReader(new FileReader(f1));
+				String lineToParse;
 				
-				String tempF = location+"/"+list[i].getName();
+				while((lineToParse = reader.readLine()) != null){
+					String[] parsed = lineToParse.split(",");
+					lv.put(parsed[0], Integer.parseInt(parsed[1]));
+				}
+				reader.close();
 				
-				//Making sure we arent comparing the same file
-				if(!f1.equals(tempF)){
-					reader = new BufferedReader(new FileReader(tempF));
+				File folder = new File(location);
+				File[] list = folder.listFiles();
+				
+				queue = new PriorityQueue<FileData>((list.length-1), comparator);
+				
+				//Comparing file with all other files
+				for (int i = 0; i < list.length; i++){
 					
-					while((lineToParse = reader.readLine()) != null){
-						String[] parsed = lineToParse.split(",");
-						rv.put(parsed[0], Integer.parseInt(parsed[1]));
+					//Clearing hashmap
+					rv.clear();
+					
+					String tempF = location+"/"+list[i].getName();
+					
+					//Making sure we arent comparing the same file
+					if(!f1.equals(tempF)){
+						reader = new BufferedReader(new FileReader(tempF));
+						
+						while((lineToParse = reader.readLine()) != null){
+							String[] parsed = lineToParse.split(",");
+							rv.put(parsed[0], Integer.parseInt(parsed[1]));
+						}
+						reader.close();
+						
+						CosineSimilarity cs = new CosineSimilarity();
+						double sim = cs.cosineSimilarity(lv,rv);
+						queue.add(new FileData(list[i].getName(), sim));
 					}
-					reader.close();
-					
-					CosineSimilarity cs = new CosineSimilarity();
-					double sim = cs.cosineSimilarity(lv,rv);
-					queue.add(new FileData(list[i].getName(), sim));
 				}
 				
-				
-				
 			}
-			
-		}
-		catch(Exception e){
-			System.out.println("Filename not valid");
-		}
-		
-		System.out.println("\nTop 20 files with highest similarity to "+f1+":\n");
-		for(int i = 0; i < 20; i++){
-			FileData tempfd = queue.remove();
-			System.out.println(tempfd.name+" "+tempfd.similarity);
+			catch(Exception e){
+				System.out.println("Filename not valid");
+			}
+			try{
+				if(matchType.equals("t")){
+					
+					FileWriter fw = new FileWriter(new File("html",fileName.getName()+".html"));
+					PrintWriter pw = new PrintWriter(fw);
+						
+					pw.println("<!DOCTYPE html>");
+					pw.println("<html>");
+					pw.println("<body>");
+					pw.println("<h1>"+fileName.getName()+"</h1>");
+						
+					
+					for(int i = 0; i < data; i++){
+						FileData tempfd = queue.remove();
+						pw.println("<a href=\""+tempfd.name+".html\">"+tempfd.name+".html</a>");
+						double sim = tempfd.similarity*100;
+						pw.println("&#9; "+sim+"% similar");
+						pw.println("<br/>");
+					}
+					
+					pw.println("</body>");
+					pw.println("</html>");
+					pw.close();
+				}
+				else{
+					FileWriter fw = new FileWriter(new File("html",fileName.getName()+".html"));
+					PrintWriter pw = new PrintWriter(fw);
+						
+					pw.println("<!DOCTYPE html>");
+					pw.println("<html>");
+					pw.println("<body>");
+					pw.println("<h1>"+fileName.getName()+"</h1>");
+					
+					for(int i = 0; i < queue.size(); i++){
+						FileData tempfd = queue.remove();
+						if(tempfd.similarity*100 > data){
+							pw.println("<a href=\""+tempfd.name+".html\">"+tempfd.name+".html</a>");
+							double sim = tempfd.similarity*100;
+							pw.println("&#9; "+sim+"% similar");
+							pw.println("<br/>");
+						}
+						else{
+							break;
+						}
+					}
+					pw.println("</body>");
+					pw.println("</html>");
+					pw.close();
+				}
+			}
+			catch(Exception e){
+				System.out.println(e);
+			}
 		}
 	}
 }
