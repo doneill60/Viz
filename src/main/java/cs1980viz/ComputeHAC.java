8fefb5497b784738e6de5b9a2ad81ef5c9bdee70
Kevin <kmm260@pitt.edu>
2018-03-26 18:04:48 -0400

    Pushed HAC

--- src/main/java/cs1980viz/ComputeHAC.java
+++ src/main/java/cs1980viz/ComputeHAC.java
@@ -4,7 +4,8 @@ import java.io.*;
 import java.util.Comparator;
 
 public class ComputeHAC{
-
+	private static int total = 0;
+	private static int[] roots;
 	public static void main(String[] args) throws IOException, FileNotFoundException{
 		String filename = "output/output.csv";
 		FileReader fr = new FileReader(filename);
@@ -23,8 +24,35 @@ public class ComputeHAC{
 		hac.cluster(dendrogramBuilder);
 		Dendrogram dendrogram = dendrogramBuilder.getDendrogram();
 		
+		roots = new int[count];
 		
+		DendrogramNode root = dendrogram.getRoot();
+		DendrogramNode cur = root;
+		/*count = 0;
+		/while(cur.getLeft() != null){
+			System.out.println("Main Observation: " + cur.getObservationCount() + " Left Observation: " + cur.getLeft().getObservationCount() + " Right Observation: " + cur.getRight().getObservationCount());
+			count += 1;
+			cur = cur.getLeft();
+			if(cur.getRight() == null){
+				System.out.println("Observe = " + cur.getObservationCount());
+			}
+		}*/
+		leafNodes(root);
+		System.out.println("Count = " + count);
 		
+	}
+	
+	public void leafNodes(Dendrogram n){
+		if(n.getLeft() != null){
+			leafNodes(n.getLeft());
+		}
+		if(n.getRight() != null){
+			leftNodes(n.getRight());
+		}
+		if(n.getLeft() == null && n.getRight() == null){
+			roots[total] = 1;
+			total += 1;
+		}
 	}
 
 }
